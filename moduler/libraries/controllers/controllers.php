cbd8cb2d676d6d72d7374df8153409a84eded020
muatik <muatik@gmail.com>
2011-08-27 18:12:12 +0300

    kasa'nın controllers sınıfı yazıldı

---
+++ moduler/libraries/controllers/controllers.php
@@ -0,0 +1,19 @@
+<?php
+abstract class controllers extends apage
+{
+	public function run(){
+		$this->generatedOutput=$this->loadPageLayout();
+	}
+
+	public function loadPageLayout(){
+		
+		$layout=preg_replace(
+			'/Controller$/','',get_class($this)
+		);
+		
+		if(file_exists($this->layoutsPath.$layout.'.php'))
+			return $this->loadView($layout.'.php',null);
+
+	}
+}
+?>
